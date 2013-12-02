06eeb9d2d5fd1af16ff7a4a94168c391f653ab4e
Josh Greenwald <green@Joshs-MacBook-Pro.local>
2013-12-02 17:49:03 -0600

    who wrote this crap

--- rawsock.h
+++ rawsock.h
@@ -4,7 +4,7 @@
 unsigned rawsock_get_adapter_ip(const char *ifname);
 int rawsock_get_adapter_mac(const char *ifname, unsigned char *mac);
 int get_raw_socket(char * device, int protocol);
-void read_socket(int , int );
+ssize_t read_socket(int , char * );
 void raw_send(int ,unsigned char * );
 
 #endif
