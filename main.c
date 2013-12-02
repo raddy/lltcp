1219b2a42ff76ac1bc25d5571f32581d66ee987d
Josh Greenwald <green@Joshs-MacBook-Pro.local>
2013-12-02 17:50:55 -0600

    yeah

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <linux/if_ether.h>
-
+#include <sys/types.h>
 #include "preprocess.h"
 #include "tcpmacros.h"
 #include "tcptemplate.h"
--- rawsock.h
+++ rawsock.h
@@ -1,6 +1,8 @@
 #ifndef RAWSOCK_H
 #define RAWSOCK_H
 
+#include <sys/types.h>
+
 unsigned rawsock_get_adapter_ip(const char *ifname);
 int rawsock_get_adapter_mac(const char *ifname, unsigned char *mac);
 int get_raw_socket(char * device, int protocol);
