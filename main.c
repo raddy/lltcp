6dc70aeca45b46161963d67efe5777bca8bbea6a
Josh Greenwald <green@Joshs-MacBook-Pro.local>
2013-12-04 23:44:37 -0600

    lorem

--- main.c
+++ main.c
@@ -75,7 +75,8 @@ int main(int argc, char **argv)
     // Set TCP data.
     strcpy (url, "www.google.com");  // Could be URL or IPv4 address
     strcpy (directory, "/");
-    strcpy (filename, "filename");
+    strcpy (filename, "
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     sprintf (payload, "GET %s%s HTTP/1.1\r\nHost: %s\r\n\r\n", directory, filename, url);
     payloadlen = strlen (payload);
 
